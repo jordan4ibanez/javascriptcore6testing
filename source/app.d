befcbd2978fb32b96e464c7523856787184acd41
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-20 21:26:25 -0400

    Time to figure this out

--- source/app.d
+++ source/app.d
@@ -1,5 +1,15 @@
+import javascriptcore.c.functions;
+import javascriptcore.context;
+import javascriptcore.global;
+import javascriptcore.value;
+import javascriptcore.virtual_machine;
 import std.stdio;
 
 void main() {
-	writeln("Edit source/app.d to start your project.");
+	// Auto creates a VM.
+	Context context = new Context();
+
+	// And then get the VM.
+	VirtualMachine vm = context.getVirtualMachine();
+
 }
