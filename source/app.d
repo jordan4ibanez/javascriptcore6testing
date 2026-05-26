64bddf8b4e8b0b9ad035320bd461df3c0e6e3074
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-26 00:46:02 -0400

    Sumtype the type to the thing

--- source/app.d
+++ source/app.d
@@ -66,16 +66,19 @@ void main() {
 	///   jsCode = The code to evaluate.
 	///   autoPrint = If the evaluation returns a string, should it write to the console?
 	/// Returns: The evaluated value.
-	Value eval(string jsCode, bool autoPrint = true) {
+	SumType!(Value, ExceptionWrap) eval(string jsCode, bool autoPrint = true) {
 
 		Value evaluationOutput = context.evaluate(jsCode);
 
-		// auto output = SumType!(Value, ExceptionWrap)();
+		auto output = SumType!(Value, ExceptionWrap)();
 
 		// This is how you check for errors.
 		ExceptionWrap except = context.getException();
 		if (except !is null) {
+
+			output = except;
 			writeln(except.toString_);
+			return output;
 		}
 
 		if (autoPrint && evaluationOutput.isString()) {
